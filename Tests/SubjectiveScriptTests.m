630cc149dcd618f291e47b138db7a709ed784a66
Kevin Malakoff <xmann.intl@gmail.com>
2012-08-03 17:27:03 +0900

    Bumped version

--- Tests/SubjectiveScriptTests.m
+++ Tests/SubjectiveScriptTests.m
@@ -35,7 +35,7 @@
 
 - (void)test_version
 {
-  ok(SS.VERSION.VersionGreaterThanOrEqualTo(@"0.1.0"), @"expected version");
+  ok(SS.VERSION.VersionGreaterThanOrEqualTo(@"0.1.1"), @"expected version");
 }
 
 - (void)testArray
