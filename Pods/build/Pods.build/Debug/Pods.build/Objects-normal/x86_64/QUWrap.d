e0c0240f40af296f17778cd8c4274f02b5b69432
Kevin Malakoff <xmann.intl@gmail.com>
2012-08-03 17:36:17 +0900

    Cleanup

--- Pods/build/Pods.build/Debug/Pods.build/Objects-normal/x86_64/QUWrap.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/kevin/Dev/OpenSourceProjects/Mine/Objective-C/SubjectiveScript.m/Pods/QUnit.m/Lib/QUWrap.m \
-  /Users/kevin/Dev/OpenSourceProjects/Mine/Objective-C/SubjectiveScript.m/Pods/QUnit.m/Lib/QUWrap.h
--- Pods/build/Pods.build/Debug/QUnit.m_OSX.build/Objects-normal/x86_64/PodsDummy_Pods.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/kevin/Dev/OpenSourceProjects/Mine/Objective-C/SubjectiveScript.m/Pods/PodsDummy_Pods.m
